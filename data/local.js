f3641782f734aeff9482c557b6529111303c9eab
Jason Tang <jasont@emetric.net>
2016-07-01 16:13:40 -0500

    remove error from local.js

--- data/local.js
+++ data/local.js
@@ -49,7 +49,8 @@ module.exports = {
   // If these are your db settings then you don't need to be explicit. If not
   // you can uncomment this and get more specific.
   db: {
-    uri: process.env.MONGODB_URI ?? 'mongodb://localhost:27017/apostrophe-sandbox'
+    uri: process.env.MONGODB_URI
+    // ?? 'mongodb://localhost:27017/dragons'
 
     // There is legacy support for host, port, name, user and password options,
     // but this is not necessary. They can all go in the uri option like this:
